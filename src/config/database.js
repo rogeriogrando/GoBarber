9bef454f0851905856e8cb8d7c1c066596233836
Fatec Itapetininga <fab.soft.fatecitapetininga@gmail.com>
2019-08-30 21:30:06 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '157.230.81.154',
+  port: '5434',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
