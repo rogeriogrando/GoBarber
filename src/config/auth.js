d0cfd339594c56d8a3526749a2473612a8f20f81
Rogério Grando <rgrando.unique@gmail.com>
2019-09-18 17:29:08 -0300

    Autenticação

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0c72dd5344f603e561d266949328daab',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,14 @@
 import { Router } from 'express';
-import User from './app/models/Users';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddlewarer from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddlewarer);
+
+routes.put('/users', UserController.update);
 export default routes;
