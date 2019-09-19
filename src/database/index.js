917dec8394e2203abbcab4c35c0e93cd4cd5e689
Rogério Grando <rgrando.unique@gmail.com>
2019-09-19 18:00:44 -0300

    Criação da listagem para usuarios

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/Users';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -13,7 +15,9 @@ class Database {
 
   init() {
     this.conection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.conection));
+    models
+      .map(model => model.init(this.conection))
+      .map(model => model.associate && model.associate(this.conection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddlewarer from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,4 +17,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewarer);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
 export default routes;
