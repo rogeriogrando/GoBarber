358ab4fb52144eab2731b5bd1f81d594a7501ab1
Rogério Grando <rgrando.unique@gmail.com>
2019-09-22 20:54:44 -0300

    Campos Virtuais

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -13,7 +13,7 @@ class AppointmentController {
     const { page = 1 } = req.query;
     const appointment = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       order: ['date'],
--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import authMiddlewarer from './app/middlewares/auth';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,7 @@ routes.use(authMiddlewarer);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
