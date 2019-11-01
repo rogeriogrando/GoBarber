c3220fc77148333aed54ff0d8051fc0d400e7996
Rogério Grando <rgrando.unique@gmail.com>
2019-10-31 22:27:18 -0300

    Avatar

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import authConfig from '../../config/auth';
 
 import User from '../models/Users';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -18,7 +19,16 @@ class SessionController {
     }
 
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found.' });
@@ -27,9 +37,9 @@ class SessionController {
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match.' });
     }
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/Users';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -57,8 +58,17 @@ class UserController {
       return res.status(400).json({ error: 'Password does not match.' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
-    return res.json({ id, name, email, provider });
+    await user.update(req.body);
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+    return res.json({ id, name, email, avatar });
   }
 }
 
